a05d8431f8f936c825730f4c2f1e077c4758efa5
andres.montoya <andres.montoya@iesonline.com.co>
2023-03-07 12:44:28 -0500

    chore:inicial

---
+++ src/test/java/co/com/ies/pruebas/cicd/CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.ies.pruebas.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
